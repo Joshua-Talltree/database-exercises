0e993c267469e1bf14332f7ab3e7e87d77e8b9a9
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-24 11:59:14 -0600

    First table creation for SQL Script

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+CREATE TABLE albums (
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist  VARCHAR(50) DEFAULT 'NONE',
+    name  VARCHAR(100) NOT NULL,
+    release_date INT(3) NOT NULL,
+    genre  VARCHAR(50) NOT NULL,
+    sales float NOT NULL,
+    PRIMARY KEY (id)
+);
