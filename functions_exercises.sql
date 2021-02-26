6cb7a1e3909183b4d4c7b29358e848878ea6f0dd
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-26 14:13:36 -0600

    functions exercise answer number 1.

---
+++ functions_exercises.sql
@@ -0,0 +1,40 @@
+USE employees;
+
+# Order by first name with last.
+SELECT CONCAT_WS('-', first_name, last_name)
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+
+
+# # Ordered by first name then last name.
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY first_name, last_name;
+#
+# # Ordered by last name then first name.
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+#   AND gender
+#     LIKE '%M%'
+# ORDER BY last_name, first_name;
+#
+# # Ordered by employee number.
+# SELECT *
+# FROM employees
+# WHERE last_name LIKE 'E%'
+# ORDER BY emp_no DESC;
+#
+# SELECT *
+# FROM employees
+# WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+# ORDER BY last_name, first_name DESC;
+#
+# # Ordered by DESC hire_date
+# SELECT *
+# FROM employees
+# WHERE hire_date LIKE '199%'
+#   AND birth_date LIKE '%12-25%'
+# ORDER BY birth_date, hire_date DESC;
