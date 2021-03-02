4515a611b7e2b53b61111ac3c88185babed25ee9
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-03-02 09:29:38 -0600

    Updated sub queries exercise, after walk-through

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,25 +1,27 @@
 USE employees;
 
-SELECT first_name, last_name, hire_date
+SELECT *
 FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+SELECT title, COUNT(title) AS Title
+FROM titles
 WHERE emp_no IN (
     SELECT emp_no
     FROM employees
-    WHERE hire_date > NOW() AND emp_no = 101010
-    );
-
-SELECT first_name
-FROM employees
-WHERE first_name IN (
-    SELECT first_name
-    FROM employees
     WHERE first_name = 'Aamod'
-);
+)
+GROUP BY title;
 
 SELECT first_name, last_name, gender
 FROM employees
 WHERE emp_no IN (
     SELECT emp_no
     FROM dept_manager
-    WHERE to_date > NOW() AND gender = 'F'
+    WHERE to_date > NOW()
+      AND gender = 'F'
 );
